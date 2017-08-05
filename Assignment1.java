34a1cabf3699201be7372d1362849ed7693f3947
krishnapattar <krishnapattar140@gmail.com>
2017-08-05 08:24:38 +0530

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,16 @@
+package com.stringmanuplation;
+/*
+ * Write a program to convert an int variable to String using an inbuilt function of String class.
+ */
+public class Assignment1 {//proper naming conventions is used 
+
+	public static void main(String[] args) {// Main methods starts
+		
+		int i=1234;
+		
+		String s=  new String().valueOf(i);
+		System.out.println("String is"+s);// printing 
+		
+
+	}
+}// class closed
